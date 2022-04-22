1fcd63b5c6848f8356d852874178926b8398e719
Chatchai Saratakij <csaratakij@gmail.com>
2022-04-22 23:15:09 +0700

    Add : simple control to player character

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 local tiny = require("tiny")
 local logs = {}
 
+-- Talking System
 local talkingSystem = tiny.processingSystem()
 talkingSystem.filter = tiny.requireAll("id", "name", "mass", "phrase")
 
@@ -11,8 +12,19 @@ function talkingSystem:process(e, dt)
 	print(strLogMessaage)
 end
 
-local someEntity =
-{
+-- Movement System
+local movementSystem = tiny.processingSystem()
+movementSystem.filter = tiny.requireAll("position", "moveDirection", "velocity")
+
+function movementSystem:process(e, dt)
+	e.velocity.x = (e.moveDirection.x * e.moveSpeed)
+	e.velocity.y = (e.moveDirection.y * e.moveSpeed)
+	e.position.x = e.position.x + (e.velocity.x * dt)
+	e.position.y = e.position.y + (e.velocity.y * dt)
+end
+
+-- Entity
+local someEntity = {
 	id = 0,
 	name = "John, Smith",
 	phrase = "I'm gonna save the world.",
@@ -20,35 +32,89 @@ local someEntity =
 	hariColor = "brown"
 }
 
-local world = tiny.world(talkingSystem, someEntity)
+local circleEntity = {
+	moveSpeed = 600,
+	position = {
+		x = 400,
+		y = 200
+	},
+	moveDirection = {
+		x = 0,
+		y = 0
+	},
+	velocity = {
+		x = 10,
+		y = 0
+	}
+}
+
+local testWorld = tiny.world(talkingSystem, someEntity)
+local gameWorld = tiny.world(movementSystem, circleEntity)
 
 function love.load(args)
 	print("*** Begin Loading ***")
 	love.window.setFullscreen(true)
+	--[[
+	print("*** System: Test World ***")
 	for i = 1, 10 do
-		world:update(1)
+		testWorld:update(1)
 	end
+	]]--
+	for i = 1, 10 do
+		print(("Test stdout on android (logcat) : %d"):format(i))
+	end
+	print("*** End Loading ***")
 end
 
+-- TODO : add input system
 function love.update(dt)
+	local direction = { x, y }
+
 	if love.keyboard.isDown("left") then
-		print("Pressed : left")
+		-- print("Pressed : left")
+		direction.x = -1
 	elseif love.keyboard.isDown("right") then
-		print("Pressed : right")
-	elseif love.keyboard.isDown("up") then
-		print("Pressed : up")
-	elseif love.keyboard.isDown("down") then
-		print("Pressed : down")
+		-- print("Pressed : right")
+		direction.x = 1
+	else
+		direction.x = 0
 	end
+
+	if love.keyboard.isDown("up") then
+		-- print("Pressed : up")
+		direction.y = -1
+	elseif love.keyboard.isDown("down") then
+		-- print("Pressed : down")
+		direction.y = 1
+	else
+		direction.y = 0
+	end
+
+	magnitude = math.sqrt((direction.x * direction.x) + (direction.y * direction.y))
+
+	if magnitude > 1.0 then
+		direction.x = (direction.x / magnitude)
+		direction.y = (direction.y / magnitude)
+	end
+
+	circleEntity.moveDirection = direction
+	gameWorld:update(dt)
 end
 
+-- TODO : add render system
 function love.draw()
 	love.graphics.clear(0, 0, 1, 1)
 	love.graphics.print(("FPS: %.1f"):format(love.timer.getFPS()), 20, 20)
+	love.graphics.print(("X: %.3f"):format(circleEntity.position.x), 20, 50)
+	love.graphics.print(("Y: %.3f"):format(circleEntity.position.y), 20, 80)
 
+	love.graphics.setColor(1, 1, 1)
+	love.graphics.circle("fill", circleEntity.position.x, circleEntity.position.y, 40, 100)
+	--[[
 	local offsetY = 10
 	for index, value in pairs(logs) do
 		love.graphics.print(value, 300, (100 + offsetY))
 		offsetY = offsetY + 4
 	end
+	]]--
 end
